c3b330f407476940eed488dc525a0ff4380119f1
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2026-04-14 12:20:30 -0300

    [AUTH] - feat: add RegisterRequest, LoginRequest and AuthResponse DTOs

---
+++ backend/src/main/java/com/ironvault/auth/adapter/in/web/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.ironvault.auth.adapter.in.web.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String accessToken;
+}
