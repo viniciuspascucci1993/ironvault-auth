eeec5ff98268c16c87f28960fd9ec879a7072b8e
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2026-04-14 10:21:48 -0300

    chore: initial project structure

---
+++ backend/src/main/java/com/ironvault/auth/IronvaultAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ironvault.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronvaultAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronvaultAuthApplication.class, args);
+	}
+
+}
